b2a72d17e88b2e983bf5466ff13d97a0ca386b32
payalsharmapixo <payal.sharma@pixoramagroup.com>
2023-12-22 14:29:50 +0530

    Modified Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 // Profile.js
 // import React from 'react';
-import { useState } from 'react';
+import { useState } from "react";
 //import classes from './Profile.module.scss'; // Assume CSS modules are set up
-import './Profile.scss';
-import CircularImagePicker from './CircularImagePicker';
-import StarRatingComponent from 'react-star-rating-component';
+import "./Profile.scss";
+import CircularImagePicker from "./CircularImagePicker";
+import StarRatingComponent from "react-star-rating-component";
 
 const Profile = () => {
   // const images = [
@@ -14,20 +14,20 @@ const Profile = () => {
   //   // Add more image URLs
   // ];
   const [boxes, setBoxes] = useState([]);
-  const [images, setImages] = useState(['']);
+  const [images, setImages] = useState([""]);
   //const [rating, setRating] = useState({ rating: 2 });
   const [rating, setRating] = useState(5);
   const handleImageChange = (newImage) => {
     setImages([newImage]);
   };
   function handleOnDrag(e, boxType, color) {
-    e.dataTransfer.setData('boxType', boxType);
-    e.dataTransfer.setData('color', color);
+    e.dataTransfer.setData("boxType", boxType);
+    e.dataTransfer.setData("color", color);
   }
 
   function handleOnDrop(e) {
-    const boxType = e.dataTransfer.getData('boxType');
-    const color = e.dataTransfer.getData('color');
+    const boxType = e.dataTransfer.getData("boxType");
+    const color = e.dataTransfer.getData("color");
     setBoxes([...boxes, { boxType, color }]);
   }
 
@@ -87,7 +87,7 @@ const Profile = () => {
       <div className="profile-second-main">
         <div
           className="profile-second-box"
-          style={{ background: 'orange' }}
+          style={{ background: "orange" }}
           // draggable
           // onDragStart={(e) => handleOnDrag(e, 'profile box', 'orange')}
         >
@@ -102,7 +102,7 @@ const Profile = () => {
               <div className="profile-card-top-right">
                 <h2>James Osborn</h2>
                 <div>Male, 27</div>
-                <div style={{ fontSize: '30px' }}>
+                <div style={{ fontSize: "30px" }}>
                   <StarRatingComponent
                     name="rate1"
                     starCount={5}
@@ -132,49 +132,49 @@ const Profile = () => {
         <div
           className="profile-social-box-1"
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'social 1', 'blue')}
+          onDragStart={(e) => handleOnDrag(e, "social 1", "blue")}
         >
           Social 1
         </div>
         <div
           className="profile-social-box-1"
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'social 2', 'blue')}
+          onDragStart={(e) => handleOnDrag(e, "social 2", "blue")}
         >
           Social 2
         </div>
         <div
           className="profile-social-box-1"
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'social 3', 'blue')}
+          onDragStart={(e) => handleOnDrag(e, "social 3", "blue")}
         >
           Social 3
         </div>
         <div
           className="profile-social-box-1"
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'social 4', 'blue')}
+          onDragStart={(e) => handleOnDrag(e, "social 4", "blue")}
         >
           Social 4
         </div>
       </div>
       <div
         className="profile-aboutme-main-1"
-        style={{ background: 'red', height: '60vh' }}
+        style={{ background: "red", height: "60vh" }}
       >
         <h2>My Specialization</h2>
         <div class="profile-cards-container">
           <div class="profile-cards">
-              <h2>Yoga</h2>
+            <h2>Yoga</h2>
           </div>
           <div class="profile-cards">
-              <h2>Nutrition</h2>
+            <h2>Nutrition</h2>
           </div>
           <div class="profile-cards">
-              <h2>SUP</h2>
+            <h2>SUP</h2>
           </div>
           <div class="profile-cards">
-              <h2>Stretching</h2>
+            <h2>Stretching</h2>
           </div>
         </div>
       </div>
@@ -188,7 +188,7 @@ const Profile = () => {
         <div
           className="profile-aboutme-left-box"
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'about me', 'turquoise')}
+          onDragStart={(e) => handleOnDrag(e, "about me", "turquoise")}
         >
           <h2>About me</h2>
         </div>
@@ -196,7 +196,7 @@ const Profile = () => {
           className="profile-aboutme-right-box"
           onDrop={handleOnDrop}
           onDragOver={handleDragOver}
-          style={{ background: 'white' }}
+          style={{ background: "white" }}
         >
           {boxes.map((box, index) => (
             <div
@@ -228,19 +228,17 @@ const Profile = () => {
       <div className="profile-aboutme-main">
         <div
           className="profile-aboutme-left-box"
-          style={{ background: 'green' }}
+          style={{ background: "green" }}
           draggable
-          onDragStart={(e) => handleOnDrag(e, 'contact me', 'green')}
+          onDragStart={(e) => handleOnDrag(e, "contact me", "green")}
         >
           <h2>Contact me</h2>
         </div>
         <div
           className="profile-aboutme-right-box"
-          style={{ background: 'grey' }}
+          style={{ background: "grey" }}
         ></div>
       </div>
-
-      
     </div>
   );
 };
