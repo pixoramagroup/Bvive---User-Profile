5b566a36b5074541eb71728be0a62caf39f53119
payalsharmapixo <payal.sharma@pixoramagroup.com>
2023-12-29 20:58:08 +0530

    Added carousel in blog, photo, and video section, optimised footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import "./Footer.scss"
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
@@ -11,23 +11,51 @@ const Footer = () => {
         <div className="footer-logo">
           <img src="src/assets/white_logo.png" alt="Your Logo"></img>
         </div>
-        <div style={{width:"50vh"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Facilis illum quis reprehenderit velit possimus eaque
-                  consequuntur labore quam, ducimus impedit aut amet distinctio
-                  rerum a ipsam repudiandae enim delectus sequi.</div>
+        <div style={{ width: "50vh" }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+          illum quis reprehenderit velit possimus eaque consequuntur labore
+          quam, ducimus impedit aut amet distinctio rerum a ipsam repudiandae
+          enim delectus sequi.
+        </div>
         <div className="footer-links">
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">How it works</a>
+            </li>
+            <li>
+              <a href="#">Testimonials</a>
+            </li>
+            <li>
+              <a href="#">Search for trainers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-links">
+          <ul>
+            <li>
+              <a href="#">Help Center</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
           </ul>
         </div>
         <div className="footer-social">
-          <a href="#"><FacebookIcon /></a>
-          <a href="#"><TwitterIcon /></a>
-          <a href="#"><InstagramIcon /></a>
+          <a href="#">
+            <FacebookIcon />
+          </a>
+          <a href="#">
+            <TwitterIcon />
+          </a>
+          <a href="#">
+            <InstagramIcon />
+          </a>
         </div>
       </div>
       <div className="footer-bottom">
