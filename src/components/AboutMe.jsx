fa482d60e92bf8861ade8976888fa634b20ef33c
payalsharmapixo <payal.sharma@pixoramagroup.com>
2023-12-29 13:43:48 +0530

    Changed Ui of different sections

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,42 +1,26 @@
 // AboutMe.js
-import React, { useState } from "react";
 
-const AboutMe = () => {
-  const [aboutMeContent, setAboutMeContent] = useState(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod lacus vel augue rutrum, non ultrices arcu tristique..."
-  );
+import React from 'react';
+import ImageGrid from './ImageGrid';
 
-  const handleContentChange = (e) => {
-    setAboutMeContent(e.target.textContent);
-  };
-
+const AboutMe = ({ title, images }) => {
   return (
-    <div className="profile-aboutme-left-box" draggable>
-      <h2
-        style={{
-          fontSize: "34px",
-          marginBottom: "10px",
-          color: "black",
-          marginLeft: "80px",
-          fontWeight: "500",
-          marginTop: "40px",
-        }}
-      >
-        About me
+    <div className="profile-aboutme-left-box" style={{ flex: 1, backgroundColor: "white", marginRight: "5px" }}>
+      <h2 style={{ fontSize: "34px", marginBottom: "10px", color: "black", fontWeight: "500", margin: "20px 0px 16px 17px", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <span>{title}</span>
+        <button style={{ marginLeft: "auto", color: "#333",
+    padding:"10px 20px",
+    fontSize: "16px",
+    marginRight: "30px",
+    fontWeight: "normal",
+    borderRadius: "5px",
+    border:" 1px solid grey",
+    cursor:"pointer",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    transition: "background 0.3s, color 0.3s",}}>View All {title}</button>
       </h2>
-      <div className="under-line" style={{ backgroundColor: "black" }}></div>
-      <div
-        style={{
-          textAlign: "center",
-          padding: "0 20px", 
-          marginTop:"10vh",
-          color:"black",
-        }}
-        contentEditable
-        onInput={handleContentChange}
-      >
-        {aboutMeContent}
-      </div>
+      {/* <div className="under-line" style={{ backgroundColor: "black" }}></div> */}
+      <ImageGrid images={images} />
     </div>
   );
 };
