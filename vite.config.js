525fa45a743389b88144cc2f07d03d0a2a016675
Ayush Jain <ayushjain070401@gmail.com>
2024-10-15 21:34:31 +0530

    Deploy updated build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Bvive---User-Profile/',
+  base: '/Bvive---User-Profile/', 
   plugins: [react()],
 })
